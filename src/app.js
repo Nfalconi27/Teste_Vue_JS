dc2e2defca59752683aab39130f2e23742724e91
NFalconi <nelson_falconi@hotmail.com>
2023-03-22 22:15:09 -0300

    data binding

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ const MyNameApp = {
     data(){
         return{
             name: "Nelson",
-            age: 31
+            age: 31,
+            input_name: ""
         }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault(); //evita que o dado seja enviado para o servidor
+
+            //console.log("Caiu aqui");
+            console.log(this.input_name); //this pois ele busca instância
+            
+            this.name = this.input_name;
+        }
+        
     }
 }
 
